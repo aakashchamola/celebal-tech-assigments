1a6dfcdbb665b442cd62c8d5d37eadb2185d3de7
aakashchamola <aakashchamolababa@gmail.com>
2025-06-16 00:00:36 +0530

    server port updated to 8000

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const handleRoutes = require('./src/fileRoutes');
-
-const PORT = 3000;
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {
     handleRoutes(req, res);
